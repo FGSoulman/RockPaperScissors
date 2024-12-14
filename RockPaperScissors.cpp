934d53e4be05d662748829ce2c99577fa21fd819
FGSoul <sam.herrscher@gmail.com>
2024-12-14 14:50:00 -0700

    RockPaperScissors

---
+++ RockPaperScissors.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+    int Rock = 0;
+    int Paper = 1;
+    int Scissors = 2;
+    char start;
+
+    std::cout << "Ready to Play? [Y/N]: ";
+    std::cin >> start;
+    std::cout << start;
+    if(start = 'Y'){
+        COM_Number(1);
+    };
+}
+
+int COM_Number(int num){
+    return 0;
+}
