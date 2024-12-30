6ae573659ea5d5a741a2c51362e91ddd203e7126
FGSoul <sam.herrscher@gmail.com>
2024-12-29 17:13:19 -0700

    Functions

--- rpsFunctions.hpp
+++ rpsFunctions.hpp
@@ -2,6 +2,10 @@
 
 void Play_Game(){
     std::cout << "Loading game";
+    std::cout << ".";
+    std::cout << "..";
+    std::cout << "...";
+    randNumCOM();
 }
 void randNumCOM(){
     std::cout << "Starting Game";
